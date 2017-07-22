c370221a35c76320707a82c480134019d8e5e778
Farrell Farahbod <farrellfarahbod@gmail.com>
2017-07-21 21:39:39 -0700

    Updated text for version 0.4

--- Telemetry Viewer/src/ControlsRegion.java
+++ Telemetry Viewer/src/ControlsRegion.java
@@ -1,6 +1,11 @@
+import java.awt.Desktop;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.net.URI;
+
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -92,24 +97,37 @@ public class ControlsRegion extends JPanel {
 		helpButton.addActionListener(event -> {
 			
 			JFrame parentWindow = (JFrame) SwingUtilities.windowForComponent(ControlsRegion.this);
-			String helpText = "<html><b>Telemetry Viewer v0.3 (2017-02-11)</b><br>" +
-			                  "A fast and simple tool for visualizing data received over a UART.<br><br>" +
+			String helpText = "<html><b>Telemetry Viewer v0.4 (2017-07-21)</b><br>" +
+			                  "A fast and easy tool for visualizing data received over a UART.<br><br>" +
 			                  "Step 1: Use the controls at the lower-right corner of the main window to connect to a serial port.<br>" +
 			                  "Step 2: A \"Data Structure\" window will pop up, use it to specify how your data is laid out, then click \"Done\"<br>" +
-			                  "Step 3: Click-and-drag in the grid region to place a chart.<br>" +
+			                  "Step 3: Click-and-drag in the tiles region to place a chart.<br>" +
 			                  "Step 4: A \"New Chart\" window will pop up, use it to specify the type of chart and its settings.<br>" +
 			                  "Repeat steps 3 and 4 to create more charts.<br><br>" +
 			                  "Use your scroll wheel to rewind or fast forward.<br>" +
 			                  "Use your scroll wheel while holding down Ctrl to zoom in or out.<br>" +
 			                  "Use your scroll wheel while holding down Shift to adjust display scaling.<br><br>" +
-			                  "Click the \"X\" icon at the top-right corner of any chart to remove it, or click the \"Reset\" button to remove all charts.<br>" +
+			                  "Click the x icon at the top-right corner of any chart to remove it.<br>" +
+			                  "Click the gear icon at the top-right corner of any chart to change its settings.<br><br>" +
+			                  "Click the \"Open Layout\" button to open a layout file.<br>" +
 			                  "Click the \"Save Layout\" button to save your current configuration (port settings, data structure, and chart settings) to a file.<br>" +
-			                  "Click the \"Open Layout\" button to open a layout file.<br><br>" +
-			                  "Click the \"Export CSV Log\" button to save all of your acquired samples to a CSV file.<br><br>" +
+			                  "Click the \"Export CSV Log\" button to save all of your acquired samples to a CSV file.<br>" +
+			                  "Click the \"Reset\" button to remove all charts.<br><br>" +
 			                  "This software is free and open source.<br>" +
-			                  "Author: Farrell Farahbod <a href=\"http://www.farrellf.com/TelemetryViewer/\">http://www.farrellf.com/TelemetryViewer/</a></html>";
+			                  "Author: Farrell Farahbod</html>";
 			JLabel helpLabel = new JLabel(helpText);
-			JOptionPane.showMessageDialog(parentWindow, helpLabel, "Help", JOptionPane.PLAIN_MESSAGE);
+			JButton websiteButton = new JButton("<html><a href=\"http://www.farrellf.com/TelemetryViewer/\">http://www.farrellf.com/TelemetryViewer/</a></html>");
+			websiteButton.addActionListener(new ActionListener() {
+				@Override public void actionPerformed(ActionEvent e) {
+					try { Desktop.getDesktop().browse(new URI("http://www.farrellf.com/TelemetryViewer/")); } catch(Exception ex) {}
+				}
+			});
+			JPanel panel = new JPanel();
+			panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+			panel.add(helpLabel);
+			panel.add(websiteButton);
+			panel.add(new JLabel(" "));
+			JOptionPane.showMessageDialog(parentWindow, panel, "Help", JOptionPane.PLAIN_MESSAGE);
 
 		});
 		
